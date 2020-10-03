ee6bf295601c0440fe3af1c1f6ce5df833075c70
dupi <avazshokiri@gmail.com>
2020-10-03 15:15:59 +0300

    init

--- JavaLabs9/src/labs9/company/Main.java
+++ JavaLabs9/src/labs9/company/Main.java
@@ -1,5 +1,9 @@
 package labs9.company;
 
 public class Main {
+    public static void main(String[] args) {
+        Company company = new Company();
 
+        
+    }
 }
--- JavaLabs9/src/labs9/company/dateSelector.java
+++ JavaLabs9/src/labs9/company/dateSelector.java
@@ -1,10 +1,14 @@
 package labs9.company;
 
 public class dateSelector implements employeeSelector{
+    int year;
 
+    public void setYear(int year) {
+        this.year = year;
+    }
 
     @Override
     public boolean HandleEmployees(Employee employee) {
-        return employee.getDateBirthday().getYear() > 1980;
+        return employee.getDateBirthday().getYear() > year;
     }
 }
